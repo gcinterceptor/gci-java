408299a529a19c3eceabbf426c87d809c2380bd7
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-03-23 19:28:57 -0300

    Add test path and test class. Tests are not implemented yet.

---
+++ core/src/test/java/com/gcinterceptor/core/GarbageCollectorControlInterceptorTest.java
@@ -0,0 +1,14 @@
+package com.gcinterceptor.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GarbageCollectorControlInterceptorTest {
+
+	@Test
+	public void testGarbageCollectorControlInterceptorHeapExecutor() {
+		fail("Not yet implemented");
+	}
+
+}
