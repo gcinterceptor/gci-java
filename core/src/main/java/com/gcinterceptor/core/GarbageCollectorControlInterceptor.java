464ef58cca31338c775061a7df4f3ef371d7b1d3
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-09 13:55:29 -0300

    Remove features moved to proxy from gci core and update it to be used by new proxy implementation.

--- core/src/main/java/com/gcinterceptor/core/GarbageCollectorControlInterceptor.java
+++ core/src/main/java/com/gcinterceptor/core/GarbageCollectorControlInterceptor.java
@@ -1,23 +1,9 @@
 package com.gcinterceptor.core;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.Clock;
-import java.time.Duration;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class GarbageCollectorControlInterceptor {
-	private static final int SAMPLE_HISTORY_SIZE = 5;
-	private static final String SHED_RATIO_CSV_FILE = System.getenv("SHED_RATIO_CSV_FILE");
-	private AtomicBoolean doingGC;
-	private AtomicLong incoming;
-	private AtomicLong finished;
 	private RuntimeEnvironment runtime;
-	private SheddingThreshold sheddingThreshold;
-	private Sampler sampler;
-	private BufferedWriter bw;
 
 	/**
 	 * Creates a new instance of {@code GarbageCollectorControlInterceptor}
@@ -27,36 +13,6 @@ public class GarbageCollectorControlInterceptor {
 	 */
 	public GarbageCollectorControlInterceptor(RuntimeEnvironment runtime) {
 		this.runtime = runtime;
-		this.sampler = new Sampler(SAMPLE_HISTORY_SIZE);
-		this.doingGC = new AtomicBoolean(false);
-		this.incoming = new AtomicLong();
-		this.finished = new AtomicLong();
-		this.sheddingThreshold = new SheddingThreshold();
-
-		if (SHED_RATIO_CSV_FILE != null) {
-			initiateCSVFlow();
-		}
-	}
-
-	private void initiateCSVFlow() {
-		try {
-			bw = new BufferedWriter(new FileWriter(SHED_RATIO_CSV_FILE, true));
-		} catch (IOException e) {
-			System.err.println(
-					"IOException! GarbageCollectorControlInterceptor had problems to be initiated. Maybe the file can't be opened or created.");
-		}
-
-		Runtime.getRuntime().addShutdownHook(new Thread() { // Ensure that the file will be closed at the end.
-			public void run() {
-				try {
-					bw.close();
-				} catch (IOException e) {
-					System.err.println("IOException! GarbageCollectorControlInterceptor had problems to close the BufferedWriter. It means that an I/O problem error has occurred.");
-				}
-			}
-		});
-
-		writeLine("finished", "shed");
 	}
 
 	/**
@@ -70,68 +26,13 @@ public class GarbageCollectorControlInterceptor {
 		this(new RuntimeEnvironment());
 	}
 
-	private ShedResponse shed() {
-		return new ShedResponse(true);
-	}
-
-	private void writeLine(String col1, String col2) {
-		try {
-			bw.write(col1 + "," + col2 + System.lineSeparator());
-			bw.flush();
-		} catch (IOException e) {
-			System.err.println("IOException! GarbageCollectorControlInterceptor had problems to write data. It means that an I/O error has occurred.");
-		}
-	}
-
-	private synchronized boolean shouldGC() {
-		double heapUsed = runtime.getHeapUsageSinceLastGC();
-		double avgReqHeapUsage = heapUsed / finished.get();
-		double heapUsedToProcessQueue = avgReqHeapUsage * (incoming.get()-finished.get());
-		double targetHeapToShed = sheddingThreshold.get() - heapUsedToProcessQueue;
-		return heapUsed > targetHeapToShed;
-	}
-
-	public ShedResponse before(String gciHeader) {
-		if(gciHeader != null) {
-				long shedRequests = Long.parseLong(gciHeader.substring(gciHeader.indexOf('/')+1));
-
-				// Force a garbage collect and keep the memory usage before the collection.
-				long alloc = runtime.collect();
-
-				// Update sampler and ST.
-				sampler.update(finished.get());
-		                sheddingThreshold.update(alloc, finished.get(), shedRequests);
-
-				if (SHED_RATIO_CSV_FILE != null) {
-					writeLine(String.valueOf(finished.get()), String.valueOf(shedRequests));
-				}
-
-				// Zeroing counters.
-				incoming.set(0);
-				finished.set(0);
-
-				doingGC.set(false);
-				incoming.incrementAndGet();
-				return new ShedResponse(false);
-		 }
-
-		if (!doingGC.get() && (incoming.get() + 1) % sampler.getCurrentSampleSize() == 0 && shouldGC()) {
-			doingGC.set(true);
-			return shed();
-		}
+    long getHeapUsageSinceLastGC() {
+        return this.runtime.getHeapUsageSinceLastGC();
+    }
+    
+    long collect() {
+        return this.runtime.collect();
+    }
 	
-		incoming.incrementAndGet();
-		return new ShedResponse(false);
-	}
-
-	/**
-	 * After must be called before the response is set to the client. It is strongly
-	 * recommended that this is the last method called in the request processing
-	 * chain.
-	 */
-	public void after(ShedResponse response) {
-		if (!response.shouldShed) {
-			finished.incrementAndGet();
-		}
-	}
+	
 }
