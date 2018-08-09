de5b8c54aea657347cc1cd06a6fb17a959cc8642
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-09 15:37:27 -0300

    Update spring adapter.

--- spring/src/main/java/com/gcinterceptor/spring/SpringGcInterceptor.java
+++ spring/src/main/java/com/gcinterceptor/spring/SpringGcInterceptor.java
@@ -1,15 +1,14 @@
 package com.gcinterceptor.spring;
 
-import com.gcinterceptor.core.GarbageCollectorControlInterceptor;
-import com.gcinterceptor.core.ShedResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.gcinterceptor.core.GarbageCollectorControlInterceptor;
 
 /**
  * Spring interceptor that uses {@link GarbageCollectorControlInterceptor} to
@@ -20,7 +19,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Configuration
 public class SpringGcInterceptor extends HandlerInterceptorAdapter {
 
-	private static final String SHED_RESPONSE_OBJ_NAME = "sr";
 	private static final String GCI_HEADERS_NAME = "GCI";
 
 	@Autowired
@@ -29,19 +27,24 @@ public class SpringGcInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		ShedResponse shedResponse = gci.before(request.getHeader(GCI_HEADERS_NAME));
-		if (shedResponse.shouldShed) {
-			response.setStatus(HttpStatus.SERVICE_UNAVAILABLE.value());
-			response.setContentLength(0);
+		
+		switch (request.getHeader(GCI_HEADERS_NAME)) {
+		case "":
+			return true;
+
+		case "ch":
+			response.getWriter().write((int) gci.getHeapUsageSinceLastGC());
+			response.getWriter().flush();
+			response.getWriter().close();
+			response.setStatus(HttpStatus.OK.value());
+			return false;
+			
+		default:
+			response.setStatus(HttpStatus.OK.value());
+			gci.collect();
+			return false;
 		}
-		request.getSession().setAttribute(SHED_RESPONSE_OBJ_NAME, shedResponse);
-		return !shedResponse.shouldShed;
+		
 	}
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			@Nullable ModelAndView modelAndView) throws Exception {
-		ShedResponse shedResponse = (ShedResponse) request.getSession().getAttribute(SHED_RESPONSE_OBJ_NAME);
-		gci.after(shedResponse);
-	}
 }
