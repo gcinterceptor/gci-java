d5dcab928d0f884c3f445d54627a4272de243c9b
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-09 17:52:07 -0300

    Update tests.

--- core/src/test/java/com/gcinterceptor/core/RuntimeEnvironmentTest.java
+++ core/src/test/java/com/gcinterceptor/core/RuntimeEnvironmentTest.java
@@ -5,29 +5,60 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class RuntimeEnvironmentTest {
+	
+	@Test(expected=ExceptionInInitializerError.class)
+	public void testLibgcFailedCall() {
+		RuntimeEnvironment runtime = new RuntimeEnvironment();
+		runtime.collect();
+	}
 
 	@Test
-	public void testHeap() {
+	public void testYoungHeap() {
 		FakeHeap heap = new FakeHeap();
 		RuntimeEnvironment runtime = new RuntimeEnvironment(heap);
 
-		assertEquals(0, runtime.getHeapUsageSinceLastGC());
+		assertEquals(0, runtime.getYoungHeapUsageSinceLastGC());
 		int n = 10;
 		long arrayCost = 4000016; // 4 bytes * 10^6 + 16 bytes (array default cost) = 4,000016MB.
 		for (int i = 1; i <= n; i++) {
 			heap.alloc(arrayCost);
-			assertEquals(i * arrayCost, runtime.getHeapUsageSinceLastGC());
+			assertEquals(i * arrayCost, runtime.getYoungHeapUsageSinceLastGC());
 		}
-		assertEquals(n * arrayCost, runtime.collect());
+		runtime.collect();
+		assertEquals(0, runtime.getYoungHeapUsageSinceLastGC());
 
 		for (int i = 1; i <= n; i++) {
 			heap.alloc(arrayCost);
-			assertEquals(i * arrayCost, runtime.getHeapUsageSinceLastGC());
+			assertEquals(i * arrayCost, runtime.getYoungHeapUsageSinceLastGC());
 		}
-		assertEquals(n * arrayCost, runtime.collect());
-
+		runtime.collect();
+		assertEquals(0, runtime.getYoungHeapUsageSinceLastGC());
 	}
 	
+	@Test
+	public void testTenuredHeap() {
+		FakeHeap heap = new FakeHeap();
+		RuntimeEnvironment runtime = new RuntimeEnvironment(heap);
+
+		assertEquals(0, runtime.getTenuredHeapUsageSinceLastGC());
+		int n = 10;
+		long arrayCost = 4000016; // 4 bytes * 10^6 + 16 bytes (array default cost) = 4,000016MB.
+		for (int i = 1; i <= n; i++) {
+			heap.alloc(arrayCost);
+			assertEquals(i * arrayCost, runtime.getTenuredHeapUsageSinceLastGC());
+		}
+		runtime.collect();
+		assertEquals(0, runtime.getTenuredHeapUsageSinceLastGC());
+
+		for (int i = 1; i <= n; i++) {
+			heap.alloc(arrayCost);
+			assertEquals(i * arrayCost, runtime.getTenuredHeapUsageSinceLastGC());
+		}
+		runtime.collect();
+		assertEquals(0, runtime.getTenuredHeapUsageSinceLastGC());
+
+	}
+
 	private class FakeHeap extends RuntimeEnvironment.Heap {
 		private long memoryUsedInBytes;
 		
@@ -36,7 +67,13 @@ public class RuntimeEnvironmentTest {
 		}
 		
 		@Override
-		long getUsage() {
+		long getYoungUsage() {
+			return memoryUsedInBytes;
+		}
+		
+
+		@Override
+		long getTenuredUsage() {
 			return memoryUsedInBytes;
 		}
 
