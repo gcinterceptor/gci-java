3956f84c2e9f2a187c244a3fb165cf20aec690af
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-10 10:41:38 -0300

    Remove Heap inner class from Runtime and update tests.

--- core/src/main/java/com/gcinterceptor/core/RuntimeEnvironment.java
+++ core/src/main/java/com/gcinterceptor/core/RuntimeEnvironment.java
@@ -4,79 +4,50 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 
 public class RuntimeEnvironment {
-	private Heap heap;
+	private MemoryPoolMXBean youngPool;
+	private MemoryPoolMXBean tenuredPool;
 	private long youngLastAlloc;
 	private long tenuredLastAlloc;
-	private long youngLastUsed;
-	private long tenuredLastUsed;
-
-	public RuntimeEnvironment(Heap heap) {
-		this.heap = heap;
-		youngLastAlloc = this.heap.getYoungUsage();
-		tenuredLastAlloc = this.heap.getTenuredUsage();
-	}
 
 	public RuntimeEnvironment() {
-		this(new Heap());
+		for (final MemoryPoolMXBean pool : ManagementFactory.getMemoryPoolMXBeans()) {
+			if (pool.getName().contains("Eden")) {
+				youngPool = pool;
+				continue;
+			}
+			if (pool.getName().contains("Old")) {
+				tenuredPool = pool;
+				continue;
+			}
+
+			if (youngPool != null && tenuredPool != null) {
+				break;
+			}
+		}
+		youngLastAlloc = youngPool.getUsage().getUsed();
+		tenuredLastAlloc = tenuredPool.getUsage().getUsed();
 	}
 
 	public void collect() {
-		heap.gc();
-		youngLastAlloc = heap.getYoungUsage();
-		tenuredLastAlloc = heap.getTenuredUsage();
+		GC.force();
+		youngLastAlloc = getYoungHeapUsage();
+		tenuredLastAlloc = getTenuredHeapUsage();
 	}
 
 	public long getYoungHeapUsageSinceLastGC() {
-		youngLastUsed = getYoungHeapUsage() - youngLastAlloc;
-		return youngLastUsed;
+		return getYoungHeapUsage() - youngLastAlloc;
 	}
 
 	public long getTenuredHeapUsageSinceLastGC() {
-		tenuredLastUsed = getTenuredHeapUsage() - tenuredLastAlloc;
-		return tenuredLastUsed;
+		return getTenuredHeapUsage() - tenuredLastAlloc;
 	}
 
 	private long getYoungHeapUsage() {
-		return heap.getYoungUsage();
+		return youngPool.getUsage().getUsed();
 	}
 
 	private long getTenuredHeapUsage() {
-		return heap.getTenuredUsage();
+		return tenuredPool.getUsage().getUsed();
 	}
 
-	static class Heap {
-		private MemoryPoolMXBean youngPool;
-		private MemoryPoolMXBean tenuredPool;
-
-		Heap() {
-			for (final MemoryPoolMXBean pool : ManagementFactory.getMemoryPoolMXBeans()) {
-				if (pool.getName().contains("Eden")) {
-					youngPool = pool;
-					continue;
-				}
-				if (pool.getName().contains("Old")) {
-					tenuredPool = pool;
-					continue;
-				}
-
-				if (youngPool != null && tenuredPool != null) {
-					break;
-				}
-			}
-
-		}
-
-		long getYoungUsage() {
-			return this.youngPool.getUsage().getUsed();
-		}
-
-		long getTenuredUsage() {
-			return this.tenuredPool.getUsage().getUsed();
-		}
-
-		void gc() {
-			GC.force();
-		}
-
-	}
 }
--- core/src/test/java/com/gcinterceptor/core/RuntimeEnvironmentTest.java
+++ core/src/test/java/com/gcinterceptor/core/RuntimeEnvironmentTest.java
@@ -1,95 +1,38 @@
 package com.gcinterceptor.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RuntimeEnvironmentTest {
 
+	@BeforeClass
+	public static void setUpEnvironmentTest() {
+		System.setProperty("jvmtilib", System.getProperty("user.dir") + "/src/main/java/libgc.so");
+	}
+	
+	@AfterClass
+	public static void setDownEnvironmentTest() {
+		System.clearProperty("jvmtilib");
+	}	
+	
 	@Test
 	public void testLibgcCall() {
 		RuntimeEnvironment runtime = new RuntimeEnvironment();
-		System.setProperty("jvmtilib", System.getProperty("user.dir") + "/src/main/java/libgc.so");
 		runtime.collect();
-		System.clearProperty("jvmtilib");
 	}
 
 	@Test
-	public void testYoungHeap() {
-		FakeHeap heap = new FakeHeap();
-		RuntimeEnvironment runtime = new RuntimeEnvironment(heap);
-
-		assertEquals(0, runtime.getYoungHeapUsageSinceLastGC());
-		int n = 10;
-		long arrayCost = 4000016; // 4 bytes * 10^6 + 16 bytes (array default cost) = 4,000016MB.
-		for (int i = 1; i <= n; i++) {
-			heap.alloc(arrayCost);
-			assertEquals(i * arrayCost, runtime.getYoungHeapUsageSinceLastGC());
+	public void testRuntimeNullPoolsInit() {
+		try {
+			RuntimeEnvironment runtime = new RuntimeEnvironment();
+			runtime.getTenuredHeapUsageSinceLastGC();
+			runtime.getYoungHeapUsageSinceLastGC();
+			runtime.collect();
+		} catch (NullPointerException e) {
+			fail();
 		}
-		runtime.collect();
-		assertEquals(0, runtime.getYoungHeapUsageSinceLastGC());
-
-		for (int i = 1; i <= n; i++) {
-			heap.alloc(arrayCost);
-			assertEquals(i * arrayCost, runtime.getYoungHeapUsageSinceLastGC());
-		}
-		runtime.collect();
-		assertEquals(0, runtime.getYoungHeapUsageSinceLastGC());
 	}
-
-	@Test
-	public void testTenuredHeap() {
-		FakeHeap heap = new FakeHeap();
-		RuntimeEnvironment runtime = new RuntimeEnvironment(heap);
-
-		assertEquals(0, runtime.getTenuredHeapUsageSinceLastGC());
-		int n = 10;
-		long arrayCost = 4000016; // 4 bytes * 10^6 + 16 bytes (array default cost) = 4,000016MB.
-		for (int i = 1; i <= n; i++) {
-			heap.alloc(arrayCost);
-			assertEquals(i * arrayCost, runtime.getTenuredHeapUsageSinceLastGC());
-		}
-		runtime.collect();
-		assertEquals(0, runtime.getTenuredHeapUsageSinceLastGC());
-
-		for (int i = 1; i <= n; i++) {
-			heap.alloc(arrayCost);
-			assertEquals(i * arrayCost, runtime.getTenuredHeapUsageSinceLastGC());
-		}
-		runtime.collect();
-		assertEquals(0, runtime.getTenuredHeapUsageSinceLastGC());
-
-	}
-
-	@Test
-	public void testHeapPoolLoadIsNotNull() {
-		RuntimeEnvironment.Heap heap = new RuntimeEnvironment.Heap();
-		heap.getYoungUsage();
-		heap.getTenuredUsage();
-	}
-	
-	private class FakeHeap extends RuntimeEnvironment.Heap {
-		private long memoryUsedInBytes;
-
-		void alloc(long toAlloc) {
-			memoryUsedInBytes += toAlloc;
-		}
-
-		@Override
-		long getYoungUsage() {
-			return memoryUsedInBytes;
-		}
-
-		@Override
-		long getTenuredUsage() {
-			return memoryUsedInBytes;
-		}
-
-		@Override
-		void gc() {
-			memoryUsedInBytes = 0;
-		}
-
-	}
-
 }
