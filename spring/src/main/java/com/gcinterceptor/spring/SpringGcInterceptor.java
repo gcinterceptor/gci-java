76297e611fa3f79ece65b7f4d80a608fba71782f
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-15 18:22:56 -0300

    Remove unnecessary prints.

--- spring/src/main/java/com/gcinterceptor/spring/SpringGcInterceptor.java
+++ spring/src/main/java/com/gcinterceptor/spring/SpringGcInterceptor.java
@@ -33,24 +33,16 @@ public class SpringGcInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
-		System.out.println("preHandle");
-
 		String gciHeader = request.getHeader(GCI_HEADERS_NAME);
 		if (gciHeader == null) {
-			System.out.println("NULL");
-
 			return true;
 		}
 
 		switch (gciHeader) {
 		case EMPTY_HEADER:
-			System.out.println("EMPTY");
-
 			return true;
 
 		case CH_HEADER:
-			System.out.println("CH HEADER");
-
 			String heapUsageString = Long.valueOf(runtime.getYoungHeapUsage()) + "|"
 					+ Long.valueOf(runtime.getTenuredHeapUsage());
 			response.getWriter().write(heapUsageString);
@@ -60,8 +52,6 @@ public class SpringGcInterceptor extends HandlerInterceptorAdapter {
 			return false;
 
 		default:
-			System.out.println("DEFAULT");
-	
 			runtime.collect();
 			response.setStatus(HttpStatus.OK.value());
 			return false;
