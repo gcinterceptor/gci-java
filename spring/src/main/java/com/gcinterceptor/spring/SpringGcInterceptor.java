60cc3a04b19e02f238cf4bf5023cfb02e73ea266
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-15 18:12:01 -0300

    Refact SpringGcInterceptor and update tests and pom.

--- spring/src/main/java/com/gcinterceptor/spring/SpringGcInterceptor.java
+++ spring/src/main/java/com/gcinterceptor/spring/SpringGcInterceptor.java
@@ -18,28 +18,39 @@ import com.gcinterceptor.core.RuntimeEnvironment;
  */
 @Configuration
 public class SpringGcInterceptor extends HandlerInterceptorAdapter {
-
-	private static final String EMPTY_HEADERS_NAME = "";
-	private static final String CH_HEADERS_NAME = "ch";
+	private static final String EMPTY_HEADER = "";
+	private static final String CH_HEADER = "ch";
 	private static final String GCI_HEADERS_NAME = "gci";
 
 	@Autowired
 	private RuntimeEnvironment runtime;
 
+	public SpringGcInterceptor() {
+		runtime = new RuntimeEnvironment();
+	}
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
+		System.out.println("preHandle");
+
 		String gciHeader = request.getHeader(GCI_HEADERS_NAME);
 		if (gciHeader == null) {
+			System.out.println("NULL");
+
 			return true;
 		}
-		
+
 		switch (gciHeader) {
-		case EMPTY_HEADERS_NAME:
+		case EMPTY_HEADER:
+			System.out.println("EMPTY");
+
 			return true;
 
-		case CH_HEADERS_NAME:
+		case CH_HEADER:
+			System.out.println("CH HEADER");
+
 			String heapUsageString = Long.valueOf(runtime.getYoungHeapUsage()) + "|"
 					+ Long.valueOf(runtime.getTenuredHeapUsage());
 			response.getWriter().write(heapUsageString);
@@ -49,6 +60,8 @@ public class SpringGcInterceptor extends HandlerInterceptorAdapter {
 			return false;
 
 		default:
+			System.out.println("DEFAULT");
+	
 			runtime.collect();
 			response.setStatus(HttpStatus.OK.value());
 			return false;
---
+++ spring/src/test/java/com/gcinterceptor/spring/SpringGcInterceptorTest.java
@@ -0,0 +1,62 @@
+package com.gcinterceptor.spring;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * Unit test for simple SpringGciInterceptor.
+ */
+ public class SpringGcInterceptorTest {
+
+	private static final String EMPTY_HEADERS_NAME = "";
+	private static final String CH_HEADERS_NAME = "ch";
+	private static final String GCI_HEADERS_NAME = "gci";
+	
+	private SpringGcInterceptor springGcInterceptor;
+	private MockHttpServletRequest request;
+	private MockHttpServletResponse response;
+
+	@BeforeClass
+	public static void setUpEnvironmentTest() {
+		System.setProperty("jvmtilib", System.getProperty("user.dir") + "/src/main/java/libgc.so");
+	}
+
+	@AfterClass
+	public static void setDownEnvironmentTest() {
+		System.clearProperty("jvmtilib");
+	}
+
+	@Before 
+	public void setUp() {
+		springGcInterceptor = new SpringGcInterceptor();
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+	}
+
+	@Test
+	public void testNullGCIHeader() throws Exception {
+		springGcInterceptor.preHandle(request, response, null);
+		assertEquals(0, response.getContentAsString().length());
+	}
+
+	@Test
+	public void testEmptyGCIHeader() throws Exception {
+		request.addHeader(GCI_HEADERS_NAME, EMPTY_HEADERS_NAME);
+		springGcInterceptor.preHandle(request, response, null);
+		assertEquals(0, response.getContentAsString().length());
+	}
+
+	@Test
+	public void testCHHeader() throws Exception {
+		request.addHeader(GCI_HEADERS_NAME, CH_HEADERS_NAME);
+		springGcInterceptor.preHandle(request, response, null);
+		assertTrue(0 != response.getContentAsString().length());
+	}
+}
