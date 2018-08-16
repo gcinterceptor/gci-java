b1be53eade471d9cbbedd6be507e4d41bb902dbd
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-16 16:31:13 -0300

    Fix build sequence.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Compile libgc.so
+gcc -shared -fpic -I"/usr/jdk-10.0.1/include" -I"/usr/jdk-10.0.1/include/linux" core/src/main/java/com_gcinterceptor_core_GC.c -o core/src/main/java/libgc.so
+
 # Build GCI core 
 cd core/
 rm -rf target/
@@ -10,6 +13,3 @@ cd ../
 cd spring/
 rm -rf target/
 mvn clean install  || exit $?
-
-# Compile libgc.so
-gcc -shared -fpic -I"/usr/jdk-10.0.1/include" -I"/usr/jdk-10.0.1/include/linux" core/src/main/java/com_gcinterceptor_core_GC.c -o core/src/main/java/libgc.so
