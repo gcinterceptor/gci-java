1994391e0d7962037faf1cfdeff3adc8f22728b3
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-16 16:43:51 -0300

    Generate JNI Header in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Compile libgc.so
+javac -classpath . com/gcinterceptor/core/GC.java
+javac -h c com/gcinterceptor/core/GC.java
 gcc -shared -fpic -I"/usr/jdk-10.0.1/include" -I"/usr/jdk-10.0.1/include/linux" core/src/main/java/com_gcinterceptor_core_GC.c -o core/src/main/java/libgc.so
 
 # Build GCI core 
