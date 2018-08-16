8e91c11b67a0f5fe043d395c2eaf00046b6f262a
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-16 10:42:33 -0300

    Remove unused import.

--- spring/src/test/java/com/gcinterceptor/spring/SpringGcInterceptorTest.java
+++ spring/src/test/java/com/gcinterceptor/spring/SpringGcInterceptorTest.java
@@ -1,7 +1,6 @@
 package com.gcinterceptor.spring;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.AfterClass;
