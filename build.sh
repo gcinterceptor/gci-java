c445ba5db8f4d209cf2110c03056c78b04a8dd90
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-16 16:29:18 -0300

    Update build.sh to compile libgc.so

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Build GCI core 
 cd core/
 rm -rf target/
 mvn clean install || exit $?
 cd ../
 
+# Build SpringGcInterceptor
 cd spring/
 rm -rf target/
 mvn clean install  || exit $?
+
+# Compile libgc.so
+gcc -shared -fpic -I"/usr/jdk-10.0.1/include" -I"/usr/jdk-10.0.1/include/linux" core/src/main/java/com_gcinterceptor_core_GC.c -o core/src/main/java/libgc.so
