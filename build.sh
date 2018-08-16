1325a07a26017616d93275beae93a03b0297b9a9
dfquaresma <davidferreiraquaresma@gmail.com>
2018-08-16 16:59:36 -0300

    Compile libgc.so using JAVA_HOME environment variable.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -x
 cd core/src/main/java/
 javac -classpath . com/gcinterceptor/core/GC.java
 javac -h c com/gcinterceptor/core/GC.java
-gcc -shared -fpic -I"/usr/jdk-10.0.1/include" -I"/usr/jdk-10.0.1/include/linux" ./com_gcinterceptor_core_GC.c -o ./libgc.so
+gcc -shared -fpic -I"${JAVA_HOME}include" -I"${JAVA_HOME}/include/linux" ./com_gcinterceptor_core_GC.c -o ./libgc.so
 cd ../../../../
 
 # Build GCI core 
