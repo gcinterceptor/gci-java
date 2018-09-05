75562611b9ccb55b4a724daa81800acfa80e5f6a
dfquaresma <davidferreiraquaresma@gmail.com>
2018-09-05 09:54:55 -0300

    Update gci-java build script.

--- build.sh
+++ build.sh
@@ -23,11 +23,11 @@ mvn clean install  || exit $?
 cd ../
 
 # Build GciPlugin
-cd elasticsearch-plugin/
+cd elasticsearch-6.4.0-plugin/
 rm -rf target/
 mvn clean install  || exit $?
 cd ../
-cp core/target/gci-core-0.1.jar elasticsearch-plugin/target/
-cp elasticsearch-plugin/src/main/resources/plugin-descriptor.properties elasticsearch-plugin/target/
-cd elasticsearch-plugin/target/
-zip gci-elasticsearch-plugin.zip plugin-descriptor.properties gci-core-0.1.jar  gci-elasticsearch-plugin-0.0.1-SNAPSHOT.jar
+cp core/target/gci-core-0.1.jar elasticsearch-6.4.0-plugin/target/
+cp elasticsearch-6.4.0-plugin/src/main/resources/plugin-descriptor.properties elasticsearch-6.4.0-plugin/target/
+cd elasticsearch-6.4.0-plugin/target/
+zip gci-elasticsearch-plugin.zip plugin-descriptor.properties gci-core-0.1.jar  gci-elasticsearch-6.4.0-plugin-0.0.1-SNAPSHOT.jar
