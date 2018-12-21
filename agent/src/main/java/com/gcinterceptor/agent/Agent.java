b98438904bf619d29c5ab2e2e5d82c564a43146f
Daniel Fireman <danielfireman@gmail.com>
2018-12-21 16:46:58 -0300

    Improvements to jav agent

--- agent/src/main/java/com/gcinterceptor/agent/Agent.java
+++ agent/src/main/java/com/gcinterceptor/agent/Agent.java
@@ -5,11 +5,12 @@ import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.instrument.Instrumentation;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 public class Agent {
-    private static final String CH_HEADER = "ch";
     private static final String GCI_HEADERS_NAME = "gci";
     public static final RuntimeEnvironment runtime = new RuntimeEnvironment();
 
@@ -22,33 +23,26 @@ public class Agent {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         HttpContext context = server.createContext("/");
         context.setHandler(Agent::handleRequest);
+        server.setExecutor(Executors.newSingleThreadExecutor());
         server.start();
     }
 
     private static void handleRequest(HttpExchange exchange) throws IOException {
+        System.out.println("rs:" + System.currentTimeMillis());
         String gciHeader = exchange.getRequestHeaders().getFirst(GCI_HEADERS_NAME);
-        if (gciHeader == null) {
-            System.out.println("No GCI headers");
-            exchange.sendResponseHeaders(200, 0);
-            exchange.getResponseBody().close();
-            return;
+        try (OutputStream out = exchange.getResponseBody()) {
+            boolean shouldGC = runtime.getYoungHeapUsage() >= Long.parseLong(gciHeader);
+            if (shouldGC) {
+                System.out.println("gc:" + System.currentTimeMillis());
+                runtime.collect();
+            }
+            exchange.getResponseHeaders().set("Content-Type", "text/plain");
+            exchange.getResponseHeaders().set("Content-Length", "1");
+            exchange.sendResponseHeaders(200, 1);
+            out.write(shouldGC ? 1 : 0);
+        } finally {
+            exchange.close();
         }
-        switch (gciHeader) {
-        case CH_HEADER:
-            byte[] heapUsage = new StringBuffer().append(Long.valueOf(runtime.getYoungHeapUsage())).append("|")
-                    .append(Long.valueOf(runtime.getTenuredHeapUsage())).toString().getBytes();
-            exchange.sendResponseHeaders(200, heapUsage.length);
-            exchange.getResponseBody().write(heapUsage);
-            exchange.getResponseBody().close();
-            System.out.println("CH: " + new String(heapUsage));
-            return;
-
-        default:
-            runtime.collect();
-            System.out.println("Collect");
-            exchange.sendResponseHeaders(200, 0);
-            exchange.getResponseBody().close();
-            return;
-        }
+        System.out.println("rf:" + System.currentTimeMillis());
     }
 }
